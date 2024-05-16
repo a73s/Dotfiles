e5ed6c61f2caf2765e67c72e54e9b89e769b0335
Adam Seals <adam73seals@proton.me>
2024-05-16 15:18:50 -0500

    bash config stuff, tabs to spaces in waybar config

---
+++ bashcfg.sh
@@ -0,0 +1,11 @@
+PS1='\[\e[32m\]\u@\H\[\e[0m\]:\[\e[32m\]\w\[\e[0m\]\\$ '
+
+alias server-ssh="ssh server@71.85.150.185 -p 16976"
+
+function detach() {
+    "$@" &
+}
+
+alias open_pdf="detach bash -c evince"
+alias open_image="detach bash -c loupe"
+alias open_video="detach bash -c celluloid"
