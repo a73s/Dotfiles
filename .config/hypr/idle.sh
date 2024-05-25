fa9d3d475714d40530e97a0a17a9e87c68289e34
Adam Seals <adam73seals@proton.me>
2024-05-25 15:28:26 -0500

    no more sway, waybar changes, idle changes

--- .config/hypr/idle.sh
+++ .config/hypr/idle.sh
@@ -2,8 +2,9 @@
 
 locker="swaylock -l -c 5f5f5f --show-failed-attempts --daemonize -i '$1'"
 
-swayidle -w timeout 600 'swaymsg "output * power off"' \
-	resume 'swaymsg "output * power on"' \
+swayidle -w \
+	timeout 600 'hyprctl dispatch dpms off' \
+	resume 'hyprctl dispatch dpms on' \
 	timeout 620 "$locker" \
-	timeout 900 'systemctl suspend' \
-	before-sleep "$locker"
+	#timeout 900 'systemctl suspend' \
+	#before-sleep "$locker"
