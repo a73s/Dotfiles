3cea971700c7196516e04615bdb0d2209bad36c1
adam <adam73seals@proton.me>
2024-06-25 16:42:36 -0500

    nix packages

---
+++ env.nix
@@ -0,0 +1,22 @@
+with import <nixpkgs> {}; [
+
+  fd
+  git
+  gnumake
+  cmake
+  gcc
+  llvmPackages_18.clang-unwrapped
+  highlight
+
+  hyprland
+  hyprlock
+  hypridle
+  waybar
+  wofi
+  # nerdfonts
+
+  neovim
+  ripgrep
+  tmux
+
+]
