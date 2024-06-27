290267b182774ab3e32767aea31b352e3ddd522a
Adam Seals <adam73seals@proton.me>
2024-06-26 23:56:25 -0500

    removed sway grimshot dep, switched to kde environment n stuff

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -23,7 +23,6 @@ with import <nixpkgs> {};
   home.packages = [
 
     pkgs.hello
-    # pkgs.home-manager
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
