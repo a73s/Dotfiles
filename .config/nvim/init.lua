9412aa4920eb19695a38217eb8c4a7b6b8607975
Adam Seals <adam73seals@proton.me>
2024-08-01 15:33:37 -0500

    goofed up commenting that out

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -641,83 +641,83 @@ require('lazy').setup({
 		end,
 	},
 
-	-- { -- Collection of various small independent plugins/modules
-	-- 	'echasnovski/mini.nvim',
-	-- 	config = function()
-	-- 		-- Better Around/Inside textobjects
-	-- 		--
-	-- 		-- Examples:
-	-- 		--	- va)  - [V]isually select [A]round [)]paren
-	-- 		--	- yinq - [Y]ank [I]nside [N]ext [']quote
-	-- 		--	- ci'  - [C]hange [I]nside [']quote
-	-- 		-- require('mini.ai').setup { n_lines = 500 }
-	--
-	-- 		-- Add/delete/replace surroundings (brackets, quotes, etc.)
-	-- 		--
-	-- 		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-	-- 		-- - sd'	 - [S]urround [D]elete [']quotes
-	-- 		-- - sr)'  - [S]urround [R]eplace [)] [']
-	-- 		-- require('mini.surround').setup()
-	--
-	-- 		-- Simple and easy statusline.
-	-- 		local statusline = require 'mini.statusline'
-	-- 		statusline.setup({use_icons = vim.g.have_nerd_font})
-	--
-	-- 		local map = require("mini.map")
-	-- 		map.setup(
-	-- 			{
-	-- 				-- Highlight integrations (none by default)
-	-- 				integrations = {
-	-- 					map.gen_integration.builtin_search(),
-	-- 					map.gen_integration.diff(),
-	-- 					map.gen_integration.diff(),
-	-- 					map.gen_integration.diagnostic(),
-	-- 				},
-	--
-	-- 				-- Symbols used to display data
-	-- 				symbols = {
-	-- 					-- Encode symbols. See `:h MiniMap.config` for specification and
-	-- 					-- `:h MiniMap.gen_encode_symbols` for pre-built ones.
-	-- 					-- Default: solid blocks with 3x2 resolution.
-	-- 					encode = nil,
-	--
-	-- 					-- Scrollbar parts for view and line. Use empty string to disable any.
-	-- 					scroll_line = '█',
-	-- 					scroll_view = '┃',
-	-- 				},
-	--
-	-- 				-- Window options
-	-- 				window = {
-	-- 					-- Whether window is focusable in normal way (with `wincmd` or mouse)
-	-- 					focusable = false,
-	--
-	-- 					-- Side to stick ('left' or 'right')
-	-- 					side = 'right',
-	--
-	-- 					-- Whether to show count of multiple integration highlights
-	-- 					show_integration_count = true,
-	--
-	-- 					-- Total width
-	-- 					width = 20,
-	--
-	-- 					-- Value of 'winblend' option
-	-- 					winblend = 100,
-	--
-	-- 					-- Z-index
-	-- 					zindex = 10,
-	-- 				},
-	-- 			}
-	-- 		)
-	--
-	-- 		map.open()
-	--
-	-- 		---@diagnostic disable-next-line: duplicate-set-field
-	-- 		statusline.section_location = function()
-	-- 			return '%p%%, %.l:%2L:%.c'
-	-- 		end
-	--
-	-- 	end,
-	-- },
+	{ -- Collection of various small independent plugins/modules
+		'echasnovski/mini.nvim',
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--	- va)  - [V]isually select [A]round [)]paren
+			--	- yinq - [Y]ank [I]nside [N]ext [']quote
+			--	- ci'  - [C]hange [I]nside [']quote
+			-- require('mini.ai').setup { n_lines = 500 }
+
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'	 - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			-- require('mini.surround').setup()
+
+			-- Simple and easy statusline.
+			local statusline = require 'mini.statusline'
+			statusline.setup({use_icons = vim.g.have_nerd_font})
+
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return '%p%%, %.l:%2L:%.c'
+			end
+
+			-- local map = require("mini.map")
+			-- map.setup(
+			-- 	{
+			-- 		-- Highlight integrations (none by default)
+			-- 		integrations = {
+			-- 			map.gen_integration.builtin_search(),
+			-- 			map.gen_integration.diff(),
+			-- 			map.gen_integration.diff(),
+			-- 			map.gen_integration.diagnostic(),
+			-- 		},
+			--
+			-- 		-- Symbols used to display data
+			-- 		symbols = {
+			-- 			-- Encode symbols. See `:h MiniMap.config` for specification and
+			-- 			-- `:h MiniMap.gen_encode_symbols` for pre-built ones.
+			-- 			-- Default: solid blocks with 3x2 resolution.
+			-- 			encode = nil,
+			--
+			-- 			-- Scrollbar parts for view and line. Use empty string to disable any.
+			-- 			scroll_line = '█',
+			-- 			scroll_view = '┃',
+			-- 		},
+			--
+			-- 		-- Window options
+			-- 		window = {
+			-- 			-- Whether window is focusable in normal way (with `wincmd` or mouse)
+			-- 			focusable = false,
+			--
+			-- 			-- Side to stick ('left' or 'right')
+			-- 			side = 'right',
+			--
+			-- 			-- Whether to show count of multiple integration highlights
+			-- 			show_integration_count = true,
+			--
+			-- 			-- Total width
+			-- 			width = 20,
+			--
+			-- 			-- Value of 'winblend' option
+			-- 			winblend = 100,
+			--
+			-- 			-- Z-index
+			-- 			zindex = 10,
+			-- 		},
+			-- 	}
+			-- )
+			--
+			-- map.open()
+
+		end,
+	},
 
 	{ -- Highlight, edit, and navigate code
 		'nvim-treesitter/nvim-treesitter',
