de6bc04657348130b98b5ca53c1497cd991fd1a1
Adam Seals <adam73seals@proton.me>
2025-01-26 09:43:05 -0600

    readded whichkey

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -204,24 +204,24 @@ require('lazy').setup({
 		},
 	},
 
-	-- {
-	--	'folke/which-key.nvim',
-	--	event = 'VimEnter',
-	--	config = function()
-	--		require('which-key').setup()
-	--
-	--		-- Document existing key chains
-	--		require('which-key').register {
-	--			['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-	--			['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
-	--			['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-	--			['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-	--			['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-	--			['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-	--			-- ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-	--		}
-	--	end,
-	-- },
+	{
+	'folke/which-key.nvim',
+	event = 'VimEnter',
+	config = function()
+		require('which-key').setup()
+
+		-- Document existing key chains
+		require('which-key').register {
+			['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+			['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
+			['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+			['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+			['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+			['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+			-- ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
+		}
+	end,
+	},
 
 	{
 		'nvim-telescope/telescope.nvim',
