6851553e5422e477d9e15559d114ae253fc485d6
Adam Seals <adam73seals@proton.me>
2025-02-06 16:54:01 -0600

    fish open docs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,18 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+
+    function detach;
+        command $argv &; disown
+    end
+    function image;
+        detach loupe $argv
+    end
+    function pdf;
+        detach evince $argv
+    end
+    function video;
+        detach celluloid $argv
+    end
+
     alias g="git"
     alias gs="git status"
     alias nv="nvim"
@@ -8,9 +21,6 @@ if status is-interactive
     alias ls="ls -h --color=auto --group-directories-first"
     alias list="ls -Al"
 
-    # alias pdf="detach evince"
-    # alias image="detach loupe"
-    # alias video="detach celluloid"
     alias copygithub="cat $HOME/Documents/Random/github-key.txt | wl-copy"
     alias proj="cd $HOME/Documents/Random/Projects"
 end
