3533a2d6bb2f89a596269e464c1cedc8ce6c3cfe
Adam Seals <adam73seals@proton.me>
2025-04-10 10:13:52 -0500

    broke into multiple files, added ai keybinds

---
+++ .config/nvim/lua/ai.lua
@@ -0,0 +1,97 @@
+return{
+  {
+    "olimorris/codecompanion.nvim",
+
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    keys = {
+      { '<leader>aa', '<cmd>CodeCompanionActions<CR>', desc = '[A]I, [A]ctions' },
+      { '<leader>ac', '<cmd>CodeCompanionChat<CR>', desc = '[A]I, [C]hat' },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "gemini",
+        },
+        inline = {
+          adapter = "gemini",
+        },
+        cmd = {
+          adapter = "gemini_fast",
+        },
+      },
+
+      adapters = {
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = os.getenv("GEMINI_API_KEY"),
+              model = "gemini-2.5-pro-exp-03-25",
+            },
+          })
+        end,
+        gemini_fast = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = os.getenv("GEMINI_API_KEY"),
+              model = "gemini-2.0-flash",
+            },
+          })
+        end,
+      },
+
+      prompt_library = {
+        ["Document Function"] = {
+          strategy = "inline",
+          description = "Document A Function",
+          opts = {
+            index = 1,
+            is_slash_cmd = false,
+            auto_submit = false,
+            short_name = "docs",
+          },
+          prompts = {
+            {
+              role = "user",
+              content = "I'm writing the documentation a function in my code. Can you help me write it? Here is the function I would like you to write documentation for. Please do not change the function. Please insert documentation before the function.",
+            },
+          },
+        },
+
+        ["Improve"] = {
+          strategy = "chat",
+          description = "Improve Code",
+          opts = {
+            short_name = "expert",
+            auto_submit = false,
+            stop_context_insertion = true,
+            -- user_prompt = true,
+          },
+
+          prompts = {
+            {
+              role = "system",
+              content = function(context)
+                return "I want you to act as a senior "
+                  .. context.filetype
+                  .. " developer. I will ask you specific questions and I want you to return concise explanations and codeblock examples."
+              end,
+            },
+            {
+              role = "user",
+              content = function(context)
+                local text = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+                return "I have the following code:\n\n```" .. context.filetype .. "\n" .. text .. "\n```\n\nHow can this be improved?"
+              end,
+              opts = {
+                contains_code = true,
+              }
+            },
+          },
+        },
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/basic_plugins.lua
@@ -0,0 +1,107 @@
+return{
+  {
+    'mbbill/undotree',
+    event = 'VimEnter',
+    opts = {},
+    config = function()
+      vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<CR><C-w><C-h>', { desc = 'Toggle [U]ndo Tree' })
+    end,
+  },
+
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {
+      indent = { tab_char = '┋', char = '▎' }
+    },
+  },
+
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+
+
+    config = function()
+      local harpoon = require('harpoon')
+      harpoon:setup()
+
+      vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "[H]arpoon Menu" })
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "[H]arpoon [A]dd" })
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon Select [1]" })
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon Select [2]" })
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon Select [3]" })
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon Select [4]" })
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "[H]arpoon [P]rev" })
+      -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "[H]arpoon [N]ext" })
+    end,
+  },
+
+  {
+    "dstein64/nvim-scrollview",
+    opts = {
+      -- excluded_filetypes = {'nerdtree'},
+      -- current_only = true,
+      base = 'right',
+      signs_on_startup = {'diagnostics', 'marks', 'search', 'conflicts'},
+      scrollview_line_limit = 20000,
+      diagnostics_severities = {vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN}
+    },
+  },
+
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function ()
+      -- This module contains a number of default definitions
+      local rainbow_delimiters = require 'rainbow-delimiters'
+
+      ---@type rainbow_delimiters.config
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+          vim = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+        },
+        priority = {
+          [''] = 110,
+          lua = 210,
+        },
+        highlight = {
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterRed',
+          'RainbowDelimiterBlue',
+          -- 'RainbowDelimiterViolet',
+          -- 'RainbowDelimiterOrange',
+          -- 'RainbowDelimiterGreen',
+          -- 'RainbowDelimiterCyan',
+        },
+      }
+    end,
+  },
+}
