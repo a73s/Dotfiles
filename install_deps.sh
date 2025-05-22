f3a54fb71e34c533401fc5652f7937d6fce2413a
Adam Seals <adam73seals@proton.me>
2025-05-21 21:17:46 -0500

    added dependency install script

---
+++ install_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo dnf copr enable solopasha/hyprland 
+sudo dnf install hyprland fd-find wofi ripgrep nvim hypridle waybar hyprlock hyprpaper alacritty swaybg mako network-manager-applet xfce-polkit gnome-keyring blueman brightnessctl grim slurp wireplumber playerctl
+pip install pynvim
