2814ef5d103ff76046590930ea0b6113bca38bee
Adam Seals <adam73seals@proton.me>
2025-06-20 21:46:37 -0500

    added treesitter context plugin

--- .config/nvim/lua/basic_plugins.lua
+++ .config/nvim/lua/basic_plugins.lua
@@ -33,10 +33,6 @@ return{
       vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon Select [2]" })
       vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon Select [3]" })
       vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon Select [4]" })
-
-      -- Toggle previous & next buffers stored within Harpoon list
-      -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "[H]arpoon [P]rev" })
-      -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "[H]arpoon [N]ext" })
     end,
   },
 
@@ -86,5 +82,14 @@ return{
         },
       }
     end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      min_window_height = 20,
+      separator = '⸺',
+      max_linex = 7,
+    },
   },
 }
