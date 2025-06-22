cc4b8c912f41d6ab06b758e1fdcdfc6dd94a1c23
Adam Seals <adam73seals@proton.me>
2025-06-22 18:06:27 -0500

    install deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo dnf copr enable solopasha/hyprland 
-sudo dnf install hyprland fd-find wofi ripgrep nvim hypridle waybar hyprlock hyprpaper alacritty swaybg mako network-manager-applet xfce-polkit gnome-keyring blueman brightnessctl grim slurp wireplumber playerctl xfce-polkit
+sudo dnf install hyprland fd-find wofi ripgrep nvim hypridle waybar hyprlock hyprpaper alacritty swaybg mako network-manager-applet xfce-polkit gnome-keyring blueman brightnessctl grim slurp wireplumber playerctl xfce-polkit network-manager-applet blueman git neovim fd-find ripgrep brightnessctl nextcloud-client
 pip install pynvim
+# chsh -s /usr/bin/fish
