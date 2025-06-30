225028ca9410b2f4db60c28641043e46bd5a28de
Adam Seals <adam73seals@proton.me>
2025-06-29 22:42:02 -0500

    more robust setup script

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
-sudo dnf copr enable solopasha/hyprland 
-sudo dnf install hyprland fd-find wofi ripgrep nvim hypridle waybar hyprlock alacritty swaybg mako network-manager-applet xfce-polkit gnome-keyring blueman brightnessctl grim slurp wireplumber playerctl xfce-polkit git neovim nextcloud-client nextcloud-client-nautilus pip fish
+
+# Install stuff
+sudo dnf copr enable -y solopasha/hyprland
+sudo dnf copr enable -y monkeygold/nautilus-open-any-terminal
+sudo dnf install -y hyprland fd-find wofi ripgrep nvim hypridle waybar hyprlock alacritty swaybg mako network-manager-applet xfce-polkit gnome-keyring blueman brightnessctl grim slurp wireplumber playerctl xfce-polkit git neovim nextcloud-client nextcloud-client-nautilus pip fish seahorse nautilus-open-any-terminal podman podman-compose
+
+# Set up virtualization
+sudo dnf group install -y virtualization
+sudo systemctl start libvirtd
+sudo systemctl enable libvirtd
+
+# neovim stuff
 pip install pynvim
-# chsh -s /usr/bin/fish
+nvim --headless "+Lazy! restore" +qa
+
+# Set up nautilus
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
+
+# Install nerd fonts
+cd ~/Downloads
+# release version should be updated periodically
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/CascadiaMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Ubuntu.zip
+unzip CascadiaMono.zip
+unzip Ubuntu.zip
+mv CascadiaMono ~/.local/share/fonts/
+mv Ubuntu ~/.local/share/fonts/
+rm CascadiaMono.zip
+rm Ubuntu.zip
+cd ~/
+
+# Bash config stuff
+echo "source ~/bashcfg.sh" >> ~/.bashrc
+
+# Change shell to fish
+chsh -s /usr/bin/fish
+
+# Install flatpaks
+flatpak install fedora org.keepassxc.KeePassXC -y
+
+# Finally, update and reboot
+sudo dnf update -y
+sudo systemctl reboot
